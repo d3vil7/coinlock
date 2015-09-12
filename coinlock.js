bacb8275e91877baf2ea8f56d417148f40361367
Ryan Casey <ryepdx@gmail.com>
2015-09-12 00:57:10 -0700

    Compile code at runtime.

--- coinlock.js
+++ coinlock.js
@@ -1,13 +1,38 @@
 (function () {
-    function createLock(expiration, build) {
+    var contractCode = [
+        'contract owned {',
+        '    function owned() {',
+        '        owner = msg.sender;',
+        '    }',
+        '    modifier onlyowner() { ',
+        '        if (msg.sender == owner)',
+        '            _',
+        '    }',
+        '    address owner;',
+        '}',
+        'contract CoinLock is owned {',
+        '    uint public expiration; // Timestamp in # of seconds.',
+        '    ',
+        '    function lock(uint _expiration) onlyowner returns (bool) {',
+        '        if (_expiration > block.timestamp && expiration == 0) {',
+        '            expiration = _expiration;',
+        '            return true;',
+        '        }',
+        '        return false;',
+        '    }',
+        '    function redeem() onlyowner {',
+        '        if (block.timestamp > expiration) {',
+        '            suicide(owner);',
+        '        }',
+        '    }',
+        '}'
+    ].join("\n");
+    var build = eth.compile.solidity(contractCode).CoinLock;
+
+    function createLock(expiration) {
         web3.personal.unlockAccount(web3.eth.defaultAccount);
 
-        if (!build) {
-            console.error("'build' was not set! Cancelling deployment...");
-            return;
-        }
-
-        var contractTx = web3.eth.sendTransaction({from: web3.eth.defaultAccount, data: build.contracts.CoinLock.bin, gas: 180000})
+        var contractTx = web3.eth.sendTransaction({from: web3.eth.defaultAccount, data: build.code, gas: 180000})
         console.log("Contract deployment tx: " + contractTx);
         console.log("Waiting 12 blocks to ensure we aren't on a fork.");
 
@@ -23,7 +48,7 @@
                console.log("Contract address is " + txReceipt.contractAddress);
                console.log("DO NOT LOSE THIS ADDRESS!");
 
-               var contract = web3.eth.contract(build.contracts.CoinLock.abi).at(txReceipt.contractAddress);
+               var contract = web3.eth.contract(build.info.abiDefinition).at(txReceipt.contractAddress);
                console.log("Setting lock expiration to " + dateString(expiration));
                contract.lock(expiration);
                console.log("Done. Run 'redeem.js' after the expiration to get your ether back."); 
@@ -47,12 +72,8 @@
     } else if (new Date(expiration) == 'Invalid Date') {
         console.error("Invalid timestamp: " + expiration);
     } else {
-        if (typeof(build) == "undefined") {
-            var build = {"contracts":{"CoinLock":{"abi":"[{\"constant\":true,\"inputs\":[],\"name\":\"expiration\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"type\":\"function\"},{\"constant\":false,\"inputs\":[],\"name\":\"redeem\",\"outputs\":[],\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_expiration\",\"type\":\"uint256\"}],\"name\":\"lock\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"type\":\"function\"}]\n","bin":"60606040525b33600060006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908302179055505b6101e2806100406000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900480634665096d1461004f578063be040fb014610070578063dd4670641461007d5761004d565b005b61005a6004506100a4565b6040518082815260200191505060405180910390f35b61007b600450610140565b005b61008e6004803590602001506100ad565b6040518082815260200191505060405180910390f35b60016000505481565b6000600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff16141561013a57428211801561011957506000600160005054145b1561013157816001600050819055506001905061013b565b6000905061013b565b5b919050565b600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614156101df576001600050544211156101de57600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16ff5b5b5b56"},"owned":{"abi":"[{\"inputs\":[],\"type\":\"constructor\"}]\n","bin":"60606040525b33600060006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908302179055505b600a80603e6000396000f30060606040526008565b00"}}};
-            build.contracts.CoinLock.abi = JSON.parse(build.contracts.CoinLock.abi);
-        };
         console.log("Everything looks in order.");
         console.log("Creating a lock that expires " + dateString(expiration));
-        createLock(expiration, build);
+        createLock(expiration);
     }
 })();
--- redeem.js
+++ redeem.js
@@ -1,13 +1,38 @@
 (function () {
-    function redeemLock(address, build) {
+    var contractCode = [
+        'contract owned {',
+        '    function owned() {',
+        '        owner = msg.sender;',
+        '    }',
+        '    modifier onlyowner() { ',
+        '        if (msg.sender == owner)',
+        '            _',
+        '    }',
+        '    address owner;',
+        '}',
+        'contract CoinLock is owned {',
+        '    uint public expiration; // Timestamp in # of seconds.',
+        '    ',
+        '    function lock(uint _expiration) onlyowner returns (bool) {',
+        '        if (_expiration > block.timestamp && expiration == 0) {',
+        '            expiration = _expiration;',
+        '            return true;',
+        '        }',
+        '        return false;',
+        '    }',
+        '    function redeem() onlyowner {',
+        '        if (block.timestamp > expiration) {',
+        '            suicide(owner);',
+        '        }',
+        '    }',
+        '}'
+    ].join("\n");
+    var build = eth.compile.solidity(contractCode).CoinLock;
+
+    function redeemLock(address) {
         web3.personal.unlockAccount(web3.eth.defaultAccount);
 
-        if (!build) {
-            console.error("'build' was not set! Cancelling deployment...");
-            return;
-        }
-
-        var contract = web3.eth.contract(build.contracts.CoinLock.abi).at(address);
+        var contract = web3.eth.contract(build.info.abiDefinition).at(address);
         var redemptionTx = contract.redeem({from: web3.eth.defaultAccount, gas: 180000});
         console.log("Redemption tx is " + redemptionTx);
     };
@@ -20,12 +45,8 @@
         console.error("Needs to be the same as the address you used to create the lock.");
 
     } else {
-        if (typeof(build) == "undefined") {
-            var build = {"contracts":{"CoinLock":{"abi":"[{\"constant\":true,\"inputs\":[],\"name\":\"expiration\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"type\":\"function\"},{\"constant\":false,\"inputs\":[],\"name\":\"redeem\",\"outputs\":[],\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_expiration\",\"type\":\"uint256\"}],\"name\":\"lock\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"type\":\"function\"}]\n","bin":"60606040525b33600060006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908302179055505b6101e2806100406000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900480634665096d1461004f578063be040fb014610070578063dd4670641461007d5761004d565b005b61005a6004506100a4565b6040518082815260200191505060405180910390f35b61007b600450610140565b005b61008e6004803590602001506100ad565b6040518082815260200191505060405180910390f35b60016000505481565b6000600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff16141561013a57428211801561011957506000600160005054145b1561013157816001600050819055506001905061013b565b6000905061013b565b5b919050565b600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614156101df576001600050544211156101de57600060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16ff5b5b5b56"},"owned":{"abi":"[{\"inputs\":[],\"type\":\"constructor\"}]\n","bin":"60606040525b33600060006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908302179055505b600a80603e6000396000f30060606040526008565b00"}}};
-            build.contracts.CoinLock.abi = JSON.parse(build.contracts.CoinLock.abi);
-        };
         console.log("Everything looks in order.");
         console.log("Retrieving coins at " + address);
-        redeemLock(address, build);
+        redeemLock(address);
     }
 })();
